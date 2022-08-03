6c58dcdb345ec75685437153e1626b1d25d0c9b4
Leslie Lopez <leslie2515lopez@gmail.com>
2022-08-02 21:06:58 -0500

    Added form values function

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // SET DATE INPUT MIN WITH TODAY'S DATE
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// TAKE VALUES FROM FORM INPUT
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+}
+
+// EVENT LISTENERS
+countdownForm.addEventListener("submit", updateCountdown);
